1ad072ab4edc47f5fd844ef271c4e75db0365aed
GenericNicholasChoi <nicknickchoi1@gmail.com>
2020-05-01 13:17:54 -0400

    worked on popup settings for the reader

---
+++ app/src/main/java/com/example/webnovelreader/textchanging.kt
@@ -0,0 +1,12 @@
+package com.example.webnovelreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class textchanging : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_textchanging)
+    }
+}
