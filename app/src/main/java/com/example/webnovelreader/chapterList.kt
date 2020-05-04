f0c9c097e62b5a31953bd8f76c19213abd45a0fa
GenericNicholasChoi <nicknickchoi1@gmail.com>
2020-05-04 16:14:33 -0400

    

--- app/src/main/java/com/example/webnovelreader/chapterList.kt
+++ app/src/main/java/com/example/webnovelreader/chapterList.kt
@@ -3,13 +3,13 @@ package com.example.webnovelreader
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_chapterlist.*
+import kotlinx.android.synthetic.main.activity_chapter_list.*
 
-class chapterlist : AppCompatActivity() {
+class chapterList : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chapterlist)
+        setContentView(R.layout.activity_chapter_list)
         setSupportActionBar(toolbar)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
