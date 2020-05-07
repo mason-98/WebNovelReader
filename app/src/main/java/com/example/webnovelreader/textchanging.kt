1b86ff565096d5a3dd9efbde1e808886d13921b6
GenericNicholasChoi <nicknickchoi1@gmail.com>
2020-05-07 15:46:00 -0400

    added a font selector

--- app/src/main/java/com/example/webnovelreader/textchanging.kt
+++ app/src/main/java/com/example/webnovelreader/textchanging.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
+import android.graphics.Typeface
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
 import android.preference.PreferenceManager.getDefaultSharedPreferences
 import android.text.Layout
 import android.util.Log.d
 import android.view.*
 import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -27,16 +30,19 @@ import kotlin.math.abs
 
 class textchanging : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_textchanging)
         setSupportActionBar(findViewById(R.id.readertoolbar))
         val sharedPref = getSharedPreferences("settings", Context.MODE_PRIVATE)
-        anytext.textSize = sharedPref.getInt("text_size", 14).toFloat() + 8
+        anytext.textSize = sharedPref.getInt("text_size", 14).toFloat()
+        anytext.typeface = resources.getFont(sharedPref.getInt("font", R.font.open_sans))
         val chapterUrl = intent.getStringExtra("chapter_url")
 
         anytext.setTextColor(Color.parseColor(sharedPref!!.getString("text_colour", "#FFFFFF")))
         background.setBackgroundColor(Color.parseColor(sharedPref!!.getString("background_colour","#000000")))
+
         readersettings.setOnClickListener {
             val dialog = ReaderOptions()
             dialog.show(supportFragmentManager, "ReaderOptions")
@@ -145,7 +151,21 @@ class textchanging : AppCompatActivity() {
                 }
             }
             spinner?.onItemSelectedListener = this
+            //creating font select spinnner
 
+            val fontspinner: Spinner? = dialog.findViewById(R.id.fontspinner)
+            context?.let {
+                ArrayAdapter.createFromResource(
+                    it,
+                    R.array.fontlist,
+                    android.R.layout.simple_spinner_item
+                ).also {adapter ->
+                    adapter
+                        .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    fontspinner?.adapter =adapter
+                }
+            }
+            fontspinner?.onItemSelectedListener = this
             // define the seekbar to select the text size
             val textsizeselect: SeekBar = dialog.findViewById(R.id.textsizeselector)
             // Grab default settings for displaying the example text to the user
@@ -182,51 +202,69 @@ class textchanging : AppCompatActivity() {
             TODO("Not yet implemented")
         }
 
+        @RequiresApi(Build.VERSION_CODES.O)
         override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-            val colour = p0?.getItemAtPosition(p2)
+            val item = p0?.getItemAtPosition(p2)
             val textbox = this.activity?.findViewById<TextView>(R.id.anytext)
-            val background = this.activity?.findViewById<ConstraintLayout>(R.id.background)
             val sharedPref = this.activity?.getSharedPreferences("settings", Context.MODE_PRIVATE)
             val edit = sharedPref?.edit()
-            ( p0?.getChildAt(0) as TextView).setTextColor(Color.BLACK)
-            if (colour == "Black") {
-                textbox?.setTextColor(Color.parseColor("#000000"))
-                background?.setBackgroundColor(Color.parseColor("#FFFFFF"))
-                edit?.putString("text_colour", "#000000")
-                edit?.putString("background_colour", "#FFFFFF")
-                edit?.commit()
+            (p0?.getChildAt(0) as TextView).setTextColor(Color.BLACK)
+            if (p0.id == R.id.textspinner) {
+                val background = this.activity?.findViewById<ConstraintLayout>(R.id.background)
+                if ( item == "Black") {
+                    textbox?.setTextColor(Color.parseColor("#000000"))
+                    background?.setBackgroundColor(Color.parseColor("#FFFFFF"))
+                    edit?.putString("text_colour", "#000000")
+                    edit?.putString("background_colour", "#FFFFFF")
+                    edit?.commit()
 
-            } else if (colour == "Yellow") {
-                textbox?.setTextColor(Color.parseColor("#5F4B32"))
-                background?.setBackgroundColor(Color.parseColor("#FBF0D9"))
-                edit?.putString("text_colour", "#5F4B32")
-                edit?.putString("background_colour", "#FBF0D9")
-                edit?.commit()
+                } else if (item == "Yellow") {
+                    textbox?.setTextColor(Color.parseColor("#5F4B32"))
+                    background?.setBackgroundColor(Color.parseColor("#FBF0D9"))
+                    edit?.putString("text_colour", "#5F4B32")
+                    edit?.putString("background_colour", "#FBF0D9")
+                    edit?.commit()
 
 
-            } else if (colour == "White") {
-                textbox?.setTextColor(Color.parseColor("#FFFFFF"))
-                background?.setBackgroundColor(Color.parseColor("#000000"))
-                edit?.putString("text_colour", "#FFFFFF")
-                edit?.putString("background_colour", "#000000")
-                edit?.commit()
-            } else if (colour == "Default") {
-                textbox?.setTextColor(
-                    Color.parseColor(
-                        sharedPref!!.getString(
-                            "text_colour",
-                            "#FFFFFF"
+                } else if (item == "White") {
+                    textbox?.setTextColor(Color.parseColor("#FFFFFF"))
+                    background?.setBackgroundColor(Color.parseColor("#000000"))
+                    edit?.putString("text_colour", "#FFFFFF")
+                    edit?.putString("background_colour", "#000000")
+                    edit?.commit()
+                } else if (item == "Default") {
+                    textbox?.setTextColor(
+                        Color.parseColor(
+                            sharedPref!!.getString(
+                                "text_colour",
+                                "#FFFFFF"
+                            )
                         )
                     )
-                )
-                background?.setBackgroundColor(
-                    Color.parseColor(
-                        sharedPref!!.getString(
-                            "background_colour",
-                            "#000000"
+                    background?.setBackgroundColor(
+                        Color.parseColor(
+                            sharedPref!!.getString(
+                                "background_colour",
+                                "#000000"
+                            )
                         )
                     )
-                )
+                }
+            } else if (p0.id == R.id.fontspinner) {
+                if (item == "Montserrat") {
+                    val typeFace = resources.getFont(R.font.montserrat)
+                    textbox?.typeface =typeFace
+                    edit?.putInt("font", R.font.montserrat)
+                    edit?.commit()
+                } else if (item == "Open Sans") {
+                    val typeFace = resources.getFont(R.font.open_sans)
+                    textbox?.typeface = typeFace
+                    edit?.putInt("font", R.font.open_sans)
+                    edit?.commit()
+                } else if (item == "Default") {
+                    val typeFace = resources.getFont(sharedPref!!.getInt("font", R.font.open_sans))
+                    textbox?.typeface = typeFace
+                }
             }
         }
     }
