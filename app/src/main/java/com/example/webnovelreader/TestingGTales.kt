701659d6c4e701333991ac65764cc713e43c21bd
GenericNicholasChoi <nicknickchoi1@gmail.com>
2020-05-14 13:45:09 -0400

    removed gravity tales web scraping

--- app/src/main/java/com/example/webnovelreader/TestingGTales.kt
+++ app/src/main/java/com/example/webnovelreader/TestingGTales.kt
@@ -2,9 +2,6 @@ package com.example.webnovelreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log.d
-import com.example.webnovelreader.websites.GravityTales
-
 
 
 class TestingGTales : AppCompatActivity() {
@@ -12,7 +9,5 @@ class TestingGTales : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_testing_g_tales)
-        val book =  GravityTales().scrapeBook("http://gravitytales.com/novel/age-of-adepts")
-        d("nick", book.chapterList.toString())
     }
 }
