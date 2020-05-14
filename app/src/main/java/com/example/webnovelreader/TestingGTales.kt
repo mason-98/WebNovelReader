38c796a85e0c28a03febf2a92e0ff93447888f03
GenericNicholasChoi <nicknickchoi1@gmail.com>
2020-05-14 15:38:51 -0400

    started working on new site for scraping

--- app/src/main/java/com/example/webnovelreader/TestingGTales.kt
+++ app/src/main/java/com/example/webnovelreader/TestingGTales.kt
@@ -2,6 +2,7 @@ package com.example.webnovelreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.webnovelreader.websitesimport.VeraTales
 
 
 class TestingGTales : AppCompatActivity() {
@@ -9,5 +10,6 @@ class TestingGTales : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_testing_g_tales)
+        VeraTales().scrapeBook("https://veratales.com/novel/raising-a-fox-spirit-in-my-home")
     }
 }
